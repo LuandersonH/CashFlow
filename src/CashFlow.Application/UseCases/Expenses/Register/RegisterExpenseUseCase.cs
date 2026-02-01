502e24aa834e1b90f8c255f1a5be80815c154118
LuandersonH <luandersonfamilia@gmail.com>
2026-02-01 13:28:54 -0300

    usando FluentValidation

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -15,28 +15,12 @@ public class RegisterExpenseUseCase
 
     private void Validate(RequestRegisterExpenseJson request)
     {
+        var validator = new RegisterExpenseValidator();
+        var result = validator.Validate(request);
 
-        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
-        if(titleIsEmpty)
+        if (!result.IsValid)
         {
-            throw new ArgumentException("The title is required.");
-        }
-
-        if (request.Amount <= 0)
-        {
-            throw new ArgumentException("The Amount must be greater than zero.");
-        }
-
-        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
-        if (result > 0) {
-            throw new ArgumentException("Expenses cannot be for the future");
-        }
-
-        var paymentTypeisValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-
-        if (!paymentTypeisValid)
-        {
-            throw new ArgumentException("Payment Type is invalid.");
+            throw new ArgumentException();
         }
     }
 }
