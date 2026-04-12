42f993dad095a6ed4c3f2429f0f47772b98a7eac
LuandersonH <luandersonfamilia@gmail.com>
2026-04-12 15:09:56 -0300

    Criando fonte resolver

---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportfontResolver.cs
@@ -0,0 +1,18 @@
+﻿using PdfSharp.Charting;
+using PdfSharp.Fonts;
+using static PdfSharp.Snippets.Font.SegoeWpFontResolver;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+
+public class ExpensesReportfontResolver : IFontResolver
+{
+    byte[]? IFontResolver.GetFont(string faceName)
+    {
+        throw new NotImplementedException();
+    }
+
+    FontResolverInfo? IFontResolver.ResolveTypeface(string familyName, bool bold, bool italic)
+    {               
+        return new FontResolverInfo(familyName);
+    }
+}
