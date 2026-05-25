228aea4d87b20118a7fb76b0ceccb2aee8984cff
Luanderson Henrique <luandersonegos@gmail.com>
2026-05-25 17:55:51 -0300

    Criptografando a senha para o banco de dados pt1

---
+++ src/CashFlow.Infrastructure/Security/BCrypt.cs
@@ -0,0 +1,11 @@
+﻿using CashFlow.Domain.Security;
+
+namespace CashFlow.Infrastructure.Security;
+
+public class BCrypt : IPasswordEncripter
+{
+    public string Encrypt(string password)
+    {
+        throw new NotImplementedException();
+    }
+}
