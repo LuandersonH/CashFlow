3898898303d02636b91d7f22c52a8e0ddff1232b
Luanderson Henrique <luandersonegos@gmail.com>
2026-05-26 10:52:13 -0300

    GERAR JWT - IMPLEMENTAÇÃO COMPLETA

--- src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
+++ src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -3,7 +3,8 @@ using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.User;
-using CashFlow.Domain.Security;
+using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Domain.Security.Tokens;
 using CashFlow.Exception.ExceptionsBase;
 using FluentValidation.Results;
 
@@ -15,6 +16,7 @@ public class RegisterUserUseCase : IRegisterUserUseCase
     private readonly IPasswordEncripter _passwordEncripter;
     private readonly IUserReadOnlyRepository _userReadOnlyRepository;
     private readonly IUserWriteOnlyRepository _userWriteOnlyRepository;
+    private readonly IAccessTokenGenerator _tokenGenerator;
     private readonly IUnitOfWork _unitOfWork;
 
     public RegisterUserUseCase(
@@ -22,7 +24,8 @@ public class RegisterUserUseCase : IRegisterUserUseCase
         IPasswordEncripter passwordEncripter, 
         IUserReadOnlyRepository userReadOnlyRepository,
         IUserWriteOnlyRepository userWriteOnlyRepository,
-        IUnitOfWork unitOfWork  
+        IUnitOfWork unitOfWork,
+        IAccessTokenGenerator tokenGenerator
         )
     {
         _mapper = mapper;
@@ -30,6 +33,7 @@ public class RegisterUserUseCase : IRegisterUserUseCase
         _userReadOnlyRepository = userReadOnlyRepository;
         _userWriteOnlyRepository = userWriteOnlyRepository;
         _unitOfWork = unitOfWork;
+        _tokenGenerator = tokenGenerator;
     }
 
     public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
@@ -46,6 +50,7 @@ public class RegisterUserUseCase : IRegisterUserUseCase
         return new ResponseRegisteredUserJson
         {
             Name = user.Name,
+            Token = _tokenGenerator.Generate(user)
         };
 
     }
