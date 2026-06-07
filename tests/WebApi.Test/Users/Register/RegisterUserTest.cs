4d0ce181605f170ce22896c60a6bbd3565f7c024
LuandersonH <luandersonfamilia@gmail.com>
2026-06-07 19:46:11 -0300

    Teste de integração para erros

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,4 +1,5 @@
-﻿using CommomTestUtilities.Requests;
+﻿using CashFlow.Exception;
+using CommomTestUtilities.Requests;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
@@ -33,5 +34,24 @@ public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
 
         response.RootElement.GetProperty("name").GetString().Should().Be(request.Name);
         response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Error_Empty_Name()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        errors.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(ResourceErrorMessages.NAME_REQUIRED));
     }
 }
