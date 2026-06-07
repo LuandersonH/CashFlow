b42e2388f419c1b0168f06b11882080ce70d9ae0
LuandersonH <luandersonfamilia@gmail.com>
2026-06-07 18:38:14 -0300

    Utilizando banco de dados InMemory

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -43,9 +43,9 @@ public static class DependencyInjectionExtension
 
     private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("Connection"); ;
+        var connectionString = configuration.GetConnectionString("Connection");
 
-        var serverVersion = new MySqlServerVersion(new Version(8, 0, 45));
+        var serverVersion = ServerVersion.AutoDetect(connectionString);
     
         services.AddDbContext<CashFlowDbContext>(config => config.UseMySql(connectionString, serverVersion));
     }
